d5626549fe92b22718bd3b65a8cbf734774626da
Shiva8080 <shivamdhole63@gmail.com>
2026-01-06 19:27:11 +0530

    List_Methods.py

---
+++ INTERMEDIATE PROGRAMS/List_Methods.py
@@ -0,0 +1,14 @@
+list= [0,1,2,3,4]
+print(list)
+list.append(5) # append method
+print(list)
+list.insert(5,5)# insert method
+print(list)
+list.pop(0) # pop method delete the element of the give index
+print(list)
+list.remove(3)# remove method delete the given element
+print(list)
+list.reverse() # reverse the list
+print(list)
+list.sort()# sort the list in ascending order by default
+print(list)
